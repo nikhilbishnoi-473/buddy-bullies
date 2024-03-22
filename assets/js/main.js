50b522b6d43eae78a1b18739cc638bbce21bc273
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-03-21 13:54:33 -1200

    update done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
 }
 
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -22,11 +22,10 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-        
-      
-const mytimeout = setTimeout(layout,3000)
-function  layout()
-{
-    document.getElementById("preloader").classList.remove("image")
-    document.getElementById("img").style.display ="none"
+
+
+const mytimeout = setTimeout(layout, 3000)
+function layout() {
+  document.getElementById("preloader").classList.remove("image")
+  document.getElementById("img").style.display = "none"
 }
